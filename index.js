3319577cc30555773593935180df55263c60888d
ChanPong <rlacksgh08@naver.com>
2021-06-13 01:56:03 +0900

    modify : scheme, resolver에 대한 주석 추가

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 
+// schema.graphql: 유효성 검증
+// resolvers: db 데이터 호출
 const server = new GraphQLServer({
     typeDefs: "graphql/schema.graphql",
     resolvers
