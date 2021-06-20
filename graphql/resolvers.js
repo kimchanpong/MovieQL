cbbe2189819765f27192565b0af7ff85c9445ee4
ChanPong <rlacksgh08@naver.com>
2021-06-21 00:01:18 +0900

    modify : node-fetch -> axios로 변경

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,12 @@
 // Database 호출
-import { getMovies } from "../db/db";
+import { getMovies, getMovie, getSuggestions } from "../db/db";
 
 // Database data -> transform -> resolvers
 const resolvers = {
     Query: {
-        movies: (_, {rating, limit}) => getMovies(limit, rating)
+        movies: (_, {rating, limit}) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id)
     }
 };
 
